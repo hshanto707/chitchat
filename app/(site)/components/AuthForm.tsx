e96b2fc3e5c84e40b5d24a5b5a72f005a3344e64
Hasibul Hasan <hshanto707@gmail.com>
2023-06-11 01:24:06 +0600

    authentication added

--- app/(site)/components/AuthForm.tsx
+++ app/(site)/components/AuthForm.tsx
@@ -2,16 +2,28 @@
 
 import Button from "@/app/components/Button";
 import Input from "@/app/components/input/Input";
-import { useCallback, useState } from "react";
+import { useCallback, useEffect, useState } from "react";
 import { FieldValue, FieldValues, SubmitHandler, useForm } from "react-hook-form";
 import AuthSocialButton from "./AuthSocialButton";
 import { BsGithub, BsGoogle  } from 'react-icons/bs';
+import axios from "axios";
+import { toast } from "react-hot-toast";
+import { signIn, useSession } from "next-auth/react"
+import { useRouter } from "next/navigation";
 
 type Variant = 'LOGIN' | 'REGISTER';
 
 const AuthForm = () => {
+  const session = useSession();
+  const router = useRouter();
   const [variant, setVariant] = useState<Variant>('LOGIN');
   const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    if (session?.status == 'authenticated') {
+      router.push('/users');
+    }
+  }, [session?.status, router]);
 
   const toggleVariant = useCallback(() => {
     if (variant == 'LOGIN') setVariant('REGISTER');
@@ -36,18 +48,37 @@ const AuthForm = () => {
     setIsLoading(true);
 
     if (variant == 'REGISTER') {
-      // Axios Register
+      axios.post('/api/register', data)
+        .then(() => signIn('credentials', data))
+        .catch(() => toast.error("Something went wrong!"))
+        .finally(() => setIsLoading(false))
     }
 
     if (variant == 'LOGIN') {
-      // NextAuth Signin
+      signIn('credentials', {
+        ...data,
+        redirect: false
+      })
+        .then((e) => {
+          if (e?.error) toast.error("Invalid email or password!")
+          else if (e?.ok) {
+            toast.success("Logged in!")
+            router.push('/users');
+          }
+        })
+        .finally(() => setIsLoading(false))
     }
   }
 
   const socialAction = (action: string) => {
     setIsLoading(true);
 
-    // NextAuth Social Sign in
+    signIn(action, { redirect: false })
+      .then((e) => {
+        if (e?.error) toast.error("Invalid email or password!")
+        else if (e?.ok) toast.success("Logged in!")
+      })
+      .finally(() => setIsLoading(false))
   }
 
   return (
